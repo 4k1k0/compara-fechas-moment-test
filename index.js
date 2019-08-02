522a58b10a15c2597a22d1c3a3250acdf48e8bb5
Wako <dulo.rodrigo@gmail.com>
2019-08-02 16:23:22 -0500

    lol

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const moment = require('moment')
 const origen = moment('2019-01-01 20:21:31')
 console.log('origen', origen.calendar())
 
-const maxEspera = origen.add(PLAZO_MAXIMO, 'days')
+// No se toma `origen` para aplicar el `add()`
+// Ya que modifica el objeto original en lugar de retornar uno nuevo
+const maxEspera = moment('2019-01-01 20:21:31').add(PLAZO_MAXIMO, 'days')
 console.log('maxEspera', maxEspera.calendar())
 
 const hoy = moment('2019-03-01 13:03:22')
@@ -15,6 +17,9 @@ console.log('hoy', hoy.calendar())
 
 const restantes = maxEspera.diff(hoy, 'days')
 console.log('restantes', restantes)
+
+const after = hoy.isAfter(origen)
+console.log('hoy isAfter origen?', after)
 
 if (restantes >= 0) {
   console.log('Aún se puede efectuar la compra')
